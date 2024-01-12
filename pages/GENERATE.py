7483a5a57ad45c25900ac2dff817b9acddbf9d2a
yjoonjang <yjoonjang34@gmail.com>
2024-01-12 13:34:01 +0900

    fix: 프롬프트 수정 및 기타 ui 수정

--- pages/GENERATE.py
+++ pages/GENERATE.py
@@ -13,10 +13,11 @@ st.set_page_config(
 
 ground_guideline = {
     "지원 동기": [
-        "지원하는 회사에 대한 이해와 그 회사에서 일하고 싶은 이유를 서술해 주세요.",
-        "지원하는 직무에 대한 이해와 그 직무를 수행하고 싶은 이유를 서술해 주세요.",
-        "지원하는 회사와 으로 지원 동기를 서술해 주세요.",
-        "지원하는 직무를 수행하기 위해 필요한 당신의 능력과 경험을 서술해 주세요.",
+        "지원한 회사와 직무에 대한 당신의 열정과 흥미를 구체적인 예시를 들어 서술해 주세요.",
+        "당신이 이 직무에 적합한 이유와 관련 경험을 강조하여 서술해 주세요.",
+        "이 회사를 선택한 구체적인 이유와 그것이 당신의 장기적인 경력 목표와 어떻게 연결되는지 설명해 주세요.",
+        "해당 직무나 업계에서 당신이 기여할 수 있는 독특한 점이나 스킬을 강조하여 서술해 주세요.",
+        "회사의 미션, 비전, 가치와 당신의 가치관이 어떻게 일치하는지 예시를 들어 서술해 주세요.",
     ],
     "직무 관심 계기": [
         "어떤 경험 또는 사건이 직무에 관심을 갖게 했는지 구체적으로 작성해 주세요.",
@@ -70,12 +71,14 @@ if "user_answer" not in st.session_state:
     st.session_state["user_answer"] = {}
 
 question = st.radio(
-    "답변하고자 하는 질문을 선택해주세요.",
+    "답변하고자 하는 질문을 선택하여 가이드라인을 생성해 보세요.",
     ("지원 동기", "직무 관심 계기", "회사 경력", "프로젝트 경험", "성격의 장단점", "어려움 극복 과정", "문제 해결 경험", "기타"),
 )
 
 if question == "기타":
-    question = st.text_input("질문 내용을 직접 작성해주세요.")
+    question = st.text_area(
+        label="질문 내용을 직접 작성해주세요.", placeholder="질문 내용을 직접 작성해주세요.", height=100
+    )
 
 if st.button("가이드라인 생성하기!"):
     with st.spinner("가이드라인을 생성중입니다. 잠시만 기다려주세요."):
@@ -98,66 +101,74 @@ if st.button("가이드라인 생성하기!"):
 for idx, guideline in enumerate(st.session_state["guideline_list"]):
     st.session_state["user_answer"][guideline] = st.text_area(
         label=guideline,
-        placeholder=f"{guideline}에 대한 자신의 경험을 간단하게 소개해주세요.",
+        placeholder=guideline,
         height=200,
         key=f"guideline_{idx}",  # 각 text_area에 고유한 key 제공
     )
 
 # 기업 우대사항
-favor_info = st.text_area(label="기업 공고의 우대사항을 작성해 주세요.", placeholder="우대사항", height=200)
 
+if st.session_state["guideline_list"]:
+    favor_info = st.text_area(
+        label="기업 공고의 우대사항을 작성해 주세요.", placeholder="우대사항", height=200
+    )
 
-if st.button("자기소개서 생성하기!"):
-    with st.spinner("답변을 생성중입니다. 잠시만 기다려주세요."):
-        # 답변 취합
-        saved_self_introduction = ""
+if st.session_state["user_answer"]:
+    if st.button("자기소개서 생성하기!"):
+        with st.spinner("답변을 생성중입니다. 잠시만 기다려주세요."):
+            # 답변 취합
+            saved_self_introduction = ""
 
-        for guideline in st.session_state["guideline_list"]:
-            saved_self_introduction += (
-                f'# {guideline} \n {st.session_state["user_answer"][guideline]} \n\n'
+            for guideline in st.session_state["guideline_list"]:
+                saved_self_introduction += (
+                    f'{st.session_state["user_answer"][guideline]} \n\n'
+                )
+            print(saved_self_introduction)
+
+            # vectorDB에서 유사한 데이터 검색
+            pinecone.init(
+                api_key=st.secrets["PINECONE_API_KEY"], environment="gcp-starter"
             )
-        print(saved_self_introduction)
+            index = pinecone.Index("resumai-self-introduction-index")
 
-        # vectorDB에서 유사한 데이터 검색
-        pinecone.init(api_key=st.secrets["PINECONE_API_KEY"], environment="gcp-starter")
-        index = pinecone.Index("resumai-self-introduction-index")
+            query_embedding = get_embedding(
+                saved_self_introduction
+            )  # 유저가 질문에 답변한 것을 임베딩
+            retrieved_data = index.query(
+                vector=query_embedding, top_k=3, include_metadata=True
+            )  # 유사한 top 3개의 답변
 
-        query_embedding = get_embedding(saved_self_introduction)  # 유저가 질문에 답변한 것을 임베딩
-        retrieved_data = index.query(
-            vector=query_embedding, top_k=3, include_metadata=True
-        )  # 유사한 top 3개의 답변
+            data = retrieved_data["matches"]
 
-        data = retrieved_data["matches"]
+            data_1_question = data[0]["metadata"]["question"]
+            data_1_answer = data[0]["metadata"]["answer"]
 
-        data_1_question = data[0]["metadata"]["question"]
-        data_1_answer = data[0]["metadata"]["answer"]
+            data_2_question = data[1]["metadata"]["question"]
+            data_2_answer = data[1]["metadata"]["answer"]
 
-        data_2_question = data[1]["metadata"]["question"]
-        data_2_answer = data[1]["metadata"]["answer"]
+            data_3_question = data[2]["metadata"]["question"]
+            data_3_answer = data[2]["metadata"]["answer"]
 
-        data_3_question = data[2]["metadata"]["question"]
-        data_3_answer = data[2]["metadata"]["answer"]
+            # 프롬프트 예시
+            examples = (
+                f"예시1) \n Question: {data_1_question}, \n Answer: {data_1_answer}, \n\n "
+                f"예시2) \n Question: {data_2_question}, \n Answer: {data_2_answer}, \n\n "
+                f"예시3) \nQuestion: {data_3_question}, \n Answer: {data_3_answer}"
+            )
 
-        # 프롬프트 예시
-        examples = (
-            f"예시1) \n Question: {data_1_question}, \n Answer: {data_1_answer}, \n\n "
-            f"예시2) \n Question: {data_2_question}, \n Answer: {data_2_answer}, \n\n "
-            f"예시3) \nQuestion: {data_3_question}, \n Answer: {data_3_answer}"
-        )
+            prompt = GENERATE_SELF_INTRODUCTION_PROMPT.format(
+                favor_info=favor_info,
+                question=question,
+                context=saved_self_introduction,
+                examples=examples,
+            )
 
-        prompt = GENERATE_SELF_INTRODUCTION_PROMPT.format(
-            favor_info=favor_info,
-            question=question,
-            context=saved_self_introduction,
-            examples=examples,
-        )
+            answer = get_chat_openai(prompt)
 
-        answer = get_chat_openai(prompt)
+            if answer:
+                st.success("답변이 생성되었습니다!")
+                st.write(answer)
+            else:
+                st.error("답변 생성에 실패했습니다..")
 
-        if answer:
-            st.success("답변이 생성되었습니다!")
-            st.write(answer)
-        else:
-            st.error("답변 생성에 실패했습니다..")
-
-        print(answer)
+            print(answer)
